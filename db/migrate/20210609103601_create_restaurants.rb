96c2f7344175a3c78370cfd1f7e5fefee84d2a38
elisecouzi <elisecouzi@hotmail.com>
2021-06-09 16:54:17 +0200

    work in progress

--- db/migrate/20210609103601_create_restaurants.rb
+++ db/migrate/20210609103601_create_restaurants.rb
@@ -1,9 +1,9 @@
 class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
-      t.string :name,
-      t.string :adress,
-      t.string :phone_number,
+      t.string :name
+      t.string :address
+      t.string :phone_number
       t.string :category
 
       t.timestamps
